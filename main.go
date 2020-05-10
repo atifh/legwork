05bac1c900a696fefeece1a473c4bc1d038271c5
Atif Haider <mail@atifhaider.com>
2020-05-10 20:41:51 +0530

    Function written to create dummy users.

--- main.go
+++ main.go
@@ -8,29 +8,46 @@ import (
 	"github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"legwork/config"
-//        "github.com/Pallinder/go-randomdata"
+       "github.com/Pallinder/go-randomdata"
 
 )
 
-// func createDummyUsers() {
-// 	sqlStatement := `INSERT INTO users (name, email, age, bio, location)  VALUES ($1, $2, $3, $4) RETURNING id`
-// 	id := 0
-// 	err = db.QueryRow(sqlStatement, 30, "jon@calhoun.io", "Jonathan", "Calhoun").Scan(&id)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	fmt.Println("New record ID is:", id)
-// }
+// DB is a global variable to hold db connection
+var DB *sql.DB
+
+// ConnectDB opens a connection to the database
+func connectDB() {
+	db, err := sql.Open("postgres", config.DatabaseConfig().ConnectionString())
+	if err != nil {
+		panic(err.Error())
+	}
+
+	DB = db
+}
+
+// creates dummy users and dumps into User table
+func createDummyUsers(count int) {
+	sqlStatement := `INSERT INTO users (name, email, age, bio, location)  VALUES ($1, $2, $3, $4, $5) RETURNING id`
+	// id := 0
+	var id string
+
+	for i := 0; i < count; i++ {
+		err := DB.QueryRow(sqlStatement,
+			randomdata.SillyName(), randomdata.Email(),
+			randomdata.Number(18, 70), randomdata.Paragraph(), randomdata.City()).Scan(&id)
+
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("New User ID is:", id)
+	}
+}
 
 func main() {
 	config.LoadConfig()
+	connectDB()
 
-	db, err := sql.Open("postgres", config.DatabaseConfig().ConnectionString())
-	if err != nil {
-		panic(err)
-	}
-
-	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	driver, err := postgres.WithInstance(DB, &postgres.Config{})
 	migration, err := migrate.NewWithDatabaseInstance(
 		"file://migrations", config.DatabaseConfig().DbName(), driver)
 
@@ -45,6 +62,11 @@ func main() {
 	}
 	fmt.Println("Ran all migrations")
 
-	defer db.Close()
+	// FIXME: Should run this only when there is no user in the DB
+	fmt.Println("Creating dummy users")
+	createDummyUsers(5)
+
+	defer DB.Close()
+
 	fmt.Println("Closing DB!")
 }
