bbb995a19884600a8ac719321f4471753e06d46d
Atif Haider <mail@atifhaider.com>
2020-05-11 17:27:01 +0530

    Missing domain file added.

---
+++ domain/user.go
@@ -0,0 +1,14 @@
+package domain
+
+// import "database/sql"
+
+type User struct {
+	ID           string      `db:"id"`
+	Name         string      `db:"name"`
+	// Age          int8        `db:"age"`
+	Email        string      `db:email`
+	Bio          string      `db:"bio"`
+	Location     string      `db:"location`
+	IsActive     bool        `db:is_active`
+
+}
